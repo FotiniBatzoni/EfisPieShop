0a366ef4839c9df6b1d0840dbf2c708b9ce226c2
fotini <f.batzoni@gmail.com>
2022-12-13 19:26:49 +0200

    Creating the PieController

---
+++ EfisPieShop/Controllers/PieController.cs
@@ -0,0 +1,28 @@
+﻿using EfisPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfisPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+
+        // This is a constructor injection
+        // Because we registered the IPieRepository and the ICategoryRepository as private fields
+        // an instance of these that is of the MockPieRepository and the MockCategoryRepository will be injected
+        // So will get instances after the constructor has run
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        //Our first Action Method
+        public IActionResult List()
+        {
+            return View(_pieRepository.AllPies);  
+        }
+    }
+}
