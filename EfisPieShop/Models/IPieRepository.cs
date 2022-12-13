33ba52e972a0c5351bbf447ee8610fd6a1fef163
fotini <f.batzoni@gmail.com>
2022-12-13 18:25:18 +0200

    Creating IPieRepository

---
+++ EfisPieShop/Models/IPieRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EfisPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        Pie? GetPieById(int pieId);
+    }
+}
