86be8f0f25de37300449955ff008e96efb193388
fotini <f.batzoni@gmail.com>
2022-12-15 13:09:15 +0200

    Creating EfisPieShopDbContext

---
+++ EfisPieShop/Models/EfisPieShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfisPieShop.Models
+{
+    public class EfisPieShopDbContext : DbContext
+    {
+        public EfisPieShopDbContext(DbContextOptions<EfisPieShopDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+    }
+}
