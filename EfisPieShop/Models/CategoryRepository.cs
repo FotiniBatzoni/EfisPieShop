fe1d7f6e3d1ef1c24b117c4247975fca38f7acf7
fotini <f.batzoni@gmail.com>
2022-12-15 13:29:53 +0200

    Creating CategoryRepository

---
+++ EfisPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace EfisPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly EfisPieShopDbContext _efisPieShopDbContext;
+
+        //constructor
+        public CategoryRepository(EfisPieShopDbContext efisPieShopDbContext)
+        {
+            _efisPieShopDbContext = efisPieShopDbContext;
+        }
+
+
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return _efisPieShopDbContext.Categories.OrderBy(p => p.CategoryName);
+            }
+        }
+    }
+}
