df0edeebf3c6a708c98130914a9e07adb4b89507
fotini <f.batzoni@gmail.com>
2022-12-15 13:26:26 +0200

    Creating PieRepository

---
+++ EfisPieShop/Models/PieRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfisPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly EfisPieShopDbContext _efisPieShopDbContext;
+
+        //constructor
+        public PieRepository(EfisPieShopDbContext efisPieShopDbContext)
+        {
+            _efisPieShopDbContext = efisPieShopDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _efisPieShopDbContext.Pies.Include(c => c.Category);
+            }
+  
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _efisPieShopDbContext.Pies.Include(c => c.Category).Where(p=> (bool)p.IsPieOfTheWeek);
+            }
+
+        }
+
+        public Pie? GetPieById(int pieId)
+        { 
+             return _efisPieShopDbContext.Pies.FirstOrDefault(p=> p.PieId == pieId);
+
+        }
+    }
+}
