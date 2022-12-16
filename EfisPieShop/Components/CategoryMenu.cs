15c647dd8573557fab59b5bbf09a82e6e5bef763
fotini <f.batzoni@gmail.com>
2022-12-16 17:35:49 +0200

    Creating CategoryMenu Component

---
+++ EfisPieShop/Components/CategoryMenu.cs
@@ -0,0 +1,21 @@
+﻿using EfisPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfisPieShop.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.AllCategories.OrderBy(c => c.CategoryName);
+            return View(categories);
+        }
+    }
+}
