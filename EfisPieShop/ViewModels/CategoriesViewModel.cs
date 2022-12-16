1a053cc6d29b13e0dcd2c2da86cb7cf4609fcaef
fotini <f.batzoni@gmail.com>
2022-12-16 22:05:18 +0200

    Shop Dropdown

---
+++ EfisPieShop/ViewModels/CategoriesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using EfisPieShop.Models;
+
+namespace EfisPieShop.ViewModels
+{
+    public class CategoriesViewModel
+    {
+        public IEnumerable<Category> Cats { get; set; }
+
+
+        public CategoriesViewModel(IEnumerable<Category> cats)
+        {
+            Cats = cats;
+        }
+    }
+}
