f78d2fed3c292ba0bf4defdfbfa7cc59063642c2
fotini <f.batzoni@gmail.com>
2022-12-20 18:03:58 +0200

    Test for List_Empty_Category_ReturnsAllPies

---
+++ EfisPieShopTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using EfisPieShop.Controllers;
+using EfisPieShop.ViewModels;
+using EfisPieShopTests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EfisPieShopTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_Empty_Category_ReturnsAllPies()
+        {
+            //Arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            //Act
+            var result = pieController.List("");
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
