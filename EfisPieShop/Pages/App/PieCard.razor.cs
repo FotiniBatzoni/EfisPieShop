7af9eaca35062019099c33605f47a69e09ce3d8e
fotini <f.batzoni@gmail.com>
2022-12-22 16:04:42 +0200

    Adding Bazor for Search

---
+++ EfisPieShop/Pages/App/PieCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using EfisPieShop.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EfisPieShop.Pages.App
+{
+    public partial class PieCard
+    {
+        [Parameter]
+        public Pie? Pie { get; set; }
+    }
+}
